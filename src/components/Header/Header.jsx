b4b5f93a560997f5f48a2bac72a802ce458955cf
ivanabraz <braz.ivana@gmail.com>
2022-05-15 13:23:17 -0300

    Style adj.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -29,13 +29,13 @@ const Header = () => {
                 <HeroChild customClass='absolute w-auto 
                     h-[20rem] xs:h-[20rem] sm:h-[25rem] md:h-[25rem] lg:h-[25em] xl:h-[25rem] 2xl:h-[25rem]
                     top-[0em] xs:top-[-2rem] sm:top-0 md:top-[1em] lg:top-[1em] xl:top-[1em] 2xl:top-[1em]
-                    left-[7em] xs:left-[-1rem] sm:left-0 md:left-[10em] lg:left-[10em] xl:left-[7em] 2xl:left-[5em]
+                    left-[0rem] xs:left-[-1rem] sm:left-0 md:left-[10em] lg:left-[10em] xl:left-[7em] 2xl:left-[5em]
                     animate-wiggle ' 
                     img={weapon} alt="Ilustración" zindex="1"/>
                 <HeroChild customClass='absolute w-auto 
                     h-[20rem] xs:h-[20rem] sm:h-[40rem] md:h-[40rem] lg:h-[40em] xl:h-[40rem] 2xl:h-[40rem]
                     bottom-[2rem] 
-                    left-[7em] xs:left-[-1rem] sm:left-0 md:left-[10em] lg:left-[10em] xl:left-[3em] 2xl:left-[3em]
+                    left-[0rem] xs:left-[-1rem] sm:left-0 md:left-[10em] lg:left-[10em] xl:left-[3em] 2xl:left-[3em]
                     animate-spin-speed-45s' 
                     img={dove1} alt="Ilustración" zindex="1"/>
                 <HeroChild customClass='absolute w-auto 
@@ -60,13 +60,13 @@ const Header = () => {
                     img={earth} alt="Ilustración" zindex="1"/>
                 <HeroChild customClass='absolute w-auto 
                     h-[15rem] xs:h-[15rem] sm:h-[25rem] md:h-[25rem] lg:h-[25em] xl:h-[25rem] 2xl:h-[25rem]
-                    top-[5rem]
-                    right-[5rem]
+                    top-[1rem] xs:top-[1rem] sm:top-[1rem] md:top-[5rem] lg:top-[5rem] xl:top-[5rem] 2xl:top-[5rem]
+                    right-[0rem] xs:right-[0rem] sm:right-[0rem] md:right-[5rem] lg:right-[5rem] xl:right-[5rem] 2xl:right-[5rem]
                     animate-spin-speed-35s'
                     img={peaceSign} alt="Ilustración" zindex="1"/>
                 <HeroChild customClass='absolute w-auto 
-                    h-[20rem] xs:h-[20rem] sm:h-[25rem] md:h-[35rem] lg:h-[35em] xl:h-[40rem] 2xl:h-[40rem]
-                    bottom-5
+                    h-[25rem] xs:h-[25rem] sm:h-[25rem] md:h-[35rem] lg:h-[35em] xl:h-[40rem] 2xl:h-[40rem]
+                    bottom-5 xs:bottom-5 sm:bottom-5 md:bottom-5 lg:bottom-5 xl:bottom-5 2xl:bottom-5
                     right-[0rem] xs:right-[0rem] sm:right-[5rem] md:right-[5rem] lg:right-[5em] xl:right-[5rem] 2xl:right-[5rem]
                     animate-wiggle' 
                     img={hand} alt="Ilustración" zindex="1"/>
